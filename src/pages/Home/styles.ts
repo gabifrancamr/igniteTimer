8a69801c14dcb563b7e0af2c3f578e3132a54e6d
Gabriela França <gabrielafranca2008@hotmail.com>
2024-01-08 11:44:25 -0300

    estilização de home

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -11,43 +11,39 @@ export const HomeContainer = styled.main`
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: 3.5rem;
+    justify-content: center;
+    gap: 3rem;
   }
 `
 
 export const FormContainer = styled.div`
   width: 100%;
+  font-size: 1.125rem;
   display: flex;
-  align-items: center;
-  justify-content: center;
   gap: 0.5rem;
   color: ${(props) => props.theme['gray-100']};
-  font-size: 1.125rem;
   font-weight: bold;
   flex-wrap: wrap;
 `
 
 export const CountdownContainer = styled.div`
   font-family: 'Roboto Mono', monospace;
-  font-size: 10rem;
-  line-height: 8rem;
-  color: ${(props) => props.theme['gray-100']};
-
   display: flex;
+  font-size: 10rem;
   gap: 1rem;
+  line-height: 8rem;
 
   span {
     background-color: ${(props) => props.theme['gray-700']};
-    padding: 2rem 1rem;
     border-radius: 8px;
+    padding: 2rem 1rem;
   }
 `
 
 export const Separator = styled.div`
-  padding: 2rem;
-  color: ${(props) => props.theme['green-500']};
   width: 4rem;
-  overflow: hidden;
   display: flex;
   justify-content: center;
+  align-items: center;
+  color: ${(props) => props.theme['green-500']};
 `
