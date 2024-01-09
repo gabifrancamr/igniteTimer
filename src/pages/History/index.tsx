49f0fcc2c229b513cc3d17e13cd7711038184b53
Gabriela França <gabifmouraroc@outlook.com>
2024-01-09 11:57:00 -0300

    estilização da página history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,60 @@
+import { HistoryContent, HistoryList } from './styles'
+
 export function History() {
   return (
-    <div>
-      <h1>History</h1>
-    </div>
+    <HistoryContent>
+      <h1>Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefas</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>aula da rocket</td>
+              <td>25 minutos</td>
+              <td>há cerca de 2 meses</td>
+              <td>concluído</td>
+            </tr>
+            <tr>
+              <td>aula da rocket</td>
+              <td>25 minutos</td>
+              <td>há cerca de 2 meses</td>
+              <td>concluído</td>
+            </tr>
+            <tr>
+              <td>aula da rocket</td>
+              <td>25 minutos</td>
+              <td>há cerca de 2 meses</td>
+              <td>concluído</td>
+            </tr>
+            <tr>
+              <td>aula da rocket</td>
+              <td>25 minutos</td>
+              <td>há cerca de 2 meses</td>
+              <td>concluído</td>
+            </tr>
+            <tr>
+              <td>aula da rocket</td>
+              <td>25 minutos</td>
+              <td>há cerca de 2 meses</td>
+              <td>concluído</td>
+            </tr>
+            <tr>
+              <td>aula da rocket</td>
+              <td>25 minutos</td>
+              <td>há cerca de 2 meses</td>
+              <td>concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContent>
   )
 }
---
+++ src/pages/History/styles.ts
@@ -0,0 +1,66 @@
+import styled from 'styled-components'
+
+export const HistoryContent = styled.main`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  padding: 1.5rem;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme['gray-100']};
+  }
+`
+export const HistoryList = styled.div`
+  margin-top: 1.5rem;
+  flex: 1;
+  overflow: auto;
+  overflow-y: scroll;
+  max-height: 18rem;
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 600px;
+
+    th {
+      text-align: left;
+      color: ${(props) => props.theme['gray-100']};
+      background-color: ${(props) => props.theme['gray-600']};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        width: 50%;
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+
+    td {
+      padding: 1rem;
+      font-size: 0.875rem;
+      background-color: ${(props) => props.theme['gray-700']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
+      color: ${(props) => props.theme['gray-300']};
+      line-height: 1.6;
+
+      &:first-child {
+        padding-left: 1.5rem;
+        width: 50%;
+        border-bottom-left-radius: 8px;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
+        border-bottom-right-radius: 8px;
+      }
+    }
+  }
+`
