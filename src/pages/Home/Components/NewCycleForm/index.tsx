c92fed087210f2a06ee7d9c35422d2667da6418d
Gabriela França <gabifmouraroc@outlook.com>
2024-01-18 09:44:04 -0300

    options

--- src/pages/Home/Components/NewCycleForm/index.tsx
+++ src/pages/Home/Components/NewCycleForm/index.tsx
@@ -25,7 +25,6 @@ export function NewCycleForm() {
         <option value="Projeto 1" />
         <option value="Projeto 2" />
         <option value="Projeto 3" />
-        <option value="Banana" />
       </datalist>
 
       <label htmlFor="minutesAmount">durante</label>
